2ec83682863e3eb177f1583cfdbb8aa99ed9b6a4
Ogbonnaya Ngwu <ongwu2014@my.fit.edu>
2018-10-11 22:35:16 -0400

    data preprocessing

---
+++ 1. data-preprocessing/data_preprocessing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 18:28:25 2017
+
+@author: ogbonnayacngwu
+
+DESCRIPTION: Data set contains information of customers with the last column indicating that the customer 
+                bought a product from the company. The goal is to make the system learn the correlation of the data 
+                and is able to indicate if a future customer is will buy a product from the company or not. Here,
+                the codes preprocess the data in preparation for machine learning models
+"""
+
+#Data Preprocessing 
+
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+#Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+#Taking care of missing data (needs scikit learn)
+imputer =  Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Encoding categorical data
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+#Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#Feature scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
